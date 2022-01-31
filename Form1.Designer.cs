92678cd367aa2665bbba46be5e110f4c3fe8991f
Kwilkin <Kyle.j.wilkin@gmail.com>
2022-01-30 21:52:39 -0600

    Added rounds and high score
    Added an option to choose how many rounds there are and a recorded high score for each round.
    
    NOTE: It now adds a folder and file to your C: drive to record the high scores.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,53 +40,60 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(192, 41);
+            this.label2.Location = new System.Drawing.Point(276, 35);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(306, 37);
+            this.label2.Size = new System.Drawing.Size(251, 37);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Equation Goes Here";
+            this.label2.Text = "Are You Ready?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(198, 117);
+            this.button1.Location = new System.Drawing.Point(238, 117);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(67, 69);
             this.button1.TabIndex = 2;
             this.button1.Text = "Answer1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(311, 117);
+            this.button2.Location = new System.Drawing.Point(351, 117);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 69);
             this.button2.TabIndex = 3;
             this.button2.Text = "Answer2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(420, 117);
+            this.button3.Location = new System.Drawing.Point(460, 117);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(67, 69);
             this.button3.TabIndex = 4;
             this.button3.Text = "Answer3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Salmon;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(251, 259);
+            this.button4.Location = new System.Drawing.Point(291, 259);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(187, 90);
             this.button4.TabIndex = 5;
@@ -98,7 +105,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(610, 140);
+            this.label1.Location = new System.Drawing.Point(638, 155);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 31);
             this.label1.TabIndex = 6;
@@ -106,19 +113,19 @@
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(636, 194);
+            this.label3.Location = new System.Drawing.Point(626, 194);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 31);
+            this.label3.Size = new System.Drawing.Size(113, 31);
             this.label3.TabIndex = 7;
             this.label3.Text = "0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(48, 355);
+            this.label4.Location = new System.Drawing.Point(244, 279);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 31);
             this.label4.TabIndex = 8;
@@ -129,7 +136,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(636, 302);
+            this.label5.Location = new System.Drawing.Point(667, 302);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(29, 31);
             this.label5.TabIndex = 9;
@@ -139,7 +146,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(547, 256);
+            this.label6.Location = new System.Drawing.Point(587, 256);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(200, 31);
             this.label6.TabIndex = 10;
@@ -149,7 +156,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(610, 23);
+            this.label7.Location = new System.Drawing.Point(634, 41);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(94, 31);
             this.label7.TabIndex = 11;
@@ -157,19 +164,61 @@
             // 
             // label8
             // 
-            this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(636, 71);
+            this.label8.Location = new System.Drawing.Point(628, 79);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(29, 31);
+            this.label8.Size = new System.Drawing.Size(111, 31);
             this.label8.TabIndex = 12;
             this.label8.Text = "0";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(33, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(148, 31);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "High Score";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(33, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(148, 31);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "0";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10 Rounds",
+            "20 Rounds",
+            "30 Rounds",
+            "40 Rounds",
+            "50 Rounds",
+            "Unlimited"});
+            this.comboBox1.Location = new System.Drawing.Point(323, 155);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.Text = "20 Rounds";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(748, 409);
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(813, 409);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -203,6 +252,9 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
             label4.Visible = false;
+            if (!Directory.Exists(@"C:\MathGame"))
+            {
+                Directory.CreateDirectory(@"C:\MathGame");
+            }
+            if(File.Exists(@"C:\MathGame\HighScores.ky"))
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"C:\MathGame\HighScores.ky");
+                Console.WriteLine(lines[0]);
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(@"C:\MathGame\HighScores.ky"))
+                {
+                    sw.WriteLine("0");
+                    sw.WriteLine("0");
+                    sw.WriteLine("0");
+                    sw.WriteLine("0");
+                    sw.WriteLine("0");
+                    sw.WriteLine("0");
+                }
+            }
+            comboBox1.Text = "20 Rounds";
         }
 
         private void setTheButtons()
         {
+            int maxRounds = 0;
+            switch (comboBox1.SelectedIndex) // Could have done one if statement to set case 5 to -1 then just did if((cB1.sI + 1)*10 == MR)
+            {
+                case 0:
+                    maxRounds = 10;
+                    break;
+                case 1:
+                    maxRounds = 20;
+                    break;
+                case 2:
+                    maxRounds = 30;
+                    break;
+                case 3:
+                    maxRounds = 40;
+                    break;
+                case 4:
+                    maxRounds = 50;
+                    break;
+                case 5:
+                    maxRounds = -1;
+                    break;
+            }
+            if (System.Convert.ToInt16(label8.Text) == maxRounds)
+            {
+                label2.Text = "Game Over!!";
+                return;
+            }
             label8.Text = System.Convert.ToString(System.Convert.ToInt16(label8.Text) + 1);
             label5.Text = "3";
             button1.BackColor = SystemColors.ControlLight;
@@ -32,7 +83,7 @@ namespace WindowsFormsApp1
             label4.Text = correctAnswer.ToString();
             int incorrectAnswer1 = correctAnswer + 1;
             int incorrectAnswer2 = correctAnswer - 1;
-            label2.Text = System.Convert.ToString(var1) + '+' + System.Convert.ToString(var2);
+            label2.Text = System.Convert.ToString(var1) + " + " + System.Convert.ToString(var2);
 
             int displayChoice1 = random.Next(0, 3);
             int displayChoice2 = displayChoice1;
@@ -85,6 +136,13 @@ namespace WindowsFormsApp1
             
             //return correctAnswer;
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\MathGame\HighScores.ky");
+            label10.Text = lines[comboBox1.SelectedIndex];
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (button4.Visible)
@@ -104,15 +162,8 @@ namespace WindowsFormsApp1
                     label5.Text = System.Convert.ToString(System.Convert.ToInt16(label5.Text) - 1);
                 }
             }
-            
+            updateHighScore();
 
-        }
-
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            setTheButtons();
-            button4.Visible = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -134,7 +185,7 @@ namespace WindowsFormsApp1
                     label5.Text = System.Convert.ToString(System.Convert.ToInt16(label5.Text) - 1);
                 }
             }
-
+            updateHighScore();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -156,7 +207,32 @@ namespace WindowsFormsApp1
                     label5.Text = System.Convert.ToString(System.Convert.ToInt16(label5.Text) - 1);
                 }
             }
-
+            updateHighScore();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            setTheButtons();
+
+            button4.Visible = false;
+            comboBox1.Visible = false;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void updateHighScore()
+        {
+            
+            // Add per round high score here later. Actually probably better to switch the high score when round is changed.
+            if (System.Convert.ToInt16(label10.Text) < System.Convert.ToInt16(label3.Text))
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"C:\MathGame\HighScores.ky");
+                label10.Text = label3.Text;
+                lines[comboBox1.SelectedIndex] = label3.Text;
+                File.WriteAllLines(@"C:\MathGame\HighScores.ky", lines);
+            }
+        }
+
     }
 }
